a6db458a86582bdf2d28064c3b206e468d9812e3
shizotoaster <pron585@gmail.com>
2024-03-27 01:20:52 +0700

    * brought back the server list patch

---
+++ src/main/java/net/shizotoaster/pixelcringe/mixin/DefaultServerListMixin.java
@@ -0,0 +1,16 @@
+package net.shizotoaster.pixelcringe.mixin;
+
+import com.pixelmonmod.pixelmon.client.DefaultServerList;
+import net.shizotoaster.pixelcringe.config.PixelCringeConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultServerList.class)
+public class DefaultServerListMixin {
+    @Inject(method = "tryFetchDefaultServers(Ljava/lang/Runnable;)V", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void pixelCringe$fuckServerListModifications(Runnable onComplete, CallbackInfo ci) {
+        if (PixelCringeConfig.SERVER_LIST_PATCH.get()) ci.cancel();
+    }
+}
